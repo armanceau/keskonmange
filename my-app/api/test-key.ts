7feb5121fce81796946e8325f9d932366dea07aa
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-05-03 10:23:02 +0200

    update

--- my-app/api/test-key.ts
+++ my-app/api/test-key.ts
@@ -1,11 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse){
+export default function handler(req: VercelRequest, res: VercelResponse) {
   const apiKey = process.env.MISTRAL_API_KEY;
 
   if (!apiKey) {
     return res.status(500).json({ error: "Clé API non trouvée 🕵️‍♂️" });
   }
-
-  res.status(200).json({ key: apiKey });
+  return res.status(200).json({ key: apiKey });
 }
